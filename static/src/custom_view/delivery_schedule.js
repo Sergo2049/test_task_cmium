38c94b6de139717a35bab84ab892b9ae5f6873fa
Serhii Vydysh <helpme.bsp@gmail.com>
2024-10-11 15:28:01 +0300

    table data added

--- static/src/custom_view/delivery_schedule.js
+++ static/src/custom_view/delivery_schedule.js
@@ -58,14 +58,48 @@ export class DeliveryScheduleServices extends Component{
         console.log("week_start", week_start, "week_end",  week_end);
 
         const data = await this.orm.searchRead("sale.order", [['create_date', '>=', week_start],
-        ['create_date', '<=', week_end]], ['create_date', 'name', 'delivery_partner_id']);
-
+        ['create_date', '<=', week_end]], ['date_order', 'name', 'delivery_partner_id']);
+        
+        data.forEach(order => {
+            let object_date = new Date(order.date_order);
+            order.start_of_day = object_date.toISOString().split('T')[0];
+        })
         this.state.orm_data = data;
+        console.log("%c[data]:", "background: green", data)
 
-        this.generateOrdersTable(data, periods);
+        
+        const pivotTable = this.createPivotTable(data, periods)
+        this.generateOrdersTable(pivotTable, periods);
     }
 
-    generateOrdersTable(ordersData, periods){
+    createPivotTable(orders, dates) {
+        const pivotTable = {}; // Создаем объект для сводной таблицы
+    
+        orders.forEach(order => { // Перебираем каждый заказ в массиве orders
+            // Получаем имя партнера, если это массив, иначе используем "Без партнера"
+            const deliveryPartner = Array.isArray(order.delivery_partner_id) ? order.delivery_partner_id[1] : "No partner"; 
+            const orderDate = order.start_of_day; // Получаем дату заказа
+    
+            // Если для этого партнера еще нет записи в сводной таблице, создаем ее
+            if (!pivotTable[deliveryPartner]) {
+                pivotTable[deliveryPartner] = {}; 
+            }
+    
+            // Если для этого партнера еще нет записи по данной дате, создаем ее
+            if (!pivotTable[deliveryPartner][orderDate]) {
+                pivotTable[deliveryPartner][orderDate] = []; 
+            }
+    
+            // Добавляем номер заказа в соответствующую ячейку сводной таблицы
+            pivotTable[deliveryPartner][orderDate].push(order.name); 
+        });
+        console.log('pivot table', pivotTable)
+        return pivotTable; // Возвращаем сводную таблицу
+    }
+    
+    generateOrdersTable(pivotTable, periods){
+
+        // Create table head
         const table = document.createElement('table');
         table.classList.add('table', 'table-bordered');
         
@@ -81,25 +115,47 @@ export class DeliveryScheduleServices extends Component{
             headerRow.appendChild(th);
             
         });
-
+        
         thead.appendChild(headerRow);
         table.appendChild(thead);
+     
+        const tbody = document.createElement('tbody');
+        
+    for(const rowValue in pivotTable){
+        const tr = document.createElement('tr');
+        let trCell = document.createElement('td');
+        trCell.innerText = rowValue;
+        tr.appendChild(trCell);
 
-        // tbody = document.createElement('tbody');
+        periods.forEach(period => {
+            const orders = pivotTable[rowValue][period] || [];
+            const joined_orders = (orders.join(", ") || "");
+            console.log(joined_orders);
+            let trCell = document.createElement('td');
+            trCell.innerText = joined_orders;
+            tr.appendChild(trCell);
+        })
+        table.appendChild(tr);
+       }
 
         // Object.keys(ordersData).forEach(deliveryPartner =>{
         //     const tr = document.createElement('tr');
-
-        //     const partnerCell = document.createElement('td');
+        //     console.log('deliveryPartner', deliveryPartner);
+        //     const trCell = document.createElement('td');
         //     trCell.innerText = deliveryPartner;
         //     tr.appendChild(trCell);
-
+            
+            // periods.forEach(period => {
+            //     let td = document.createElement('td');
+            //     let order_id = ordersData[deliveryPartner][period];
+            //     console.log('order_id', order_id);
+            //     td.innerText = 
+            // })
 
             
         // })
         document.getElementById('orders-table-container').appendChild(table);
     }
-
 }
 
 DeliveryScheduleServices.template = "test_tasks.DeliveryScheduleServicesTemplate";
